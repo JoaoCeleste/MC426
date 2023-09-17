765dbc3c5f37d1d4e725d6d27dc991e6db690dc9
JoaoCeleste <141796020+JoaoCeleste@users.noreply.github.com>
2023-09-17 15:06:47 -0300

    Implementação do login com teste automatizado e as templates

---
+++ Login.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SECRET_KEY"] = "abc"
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class Users(UserMixin, db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(250), unique=True, nullable=False)
+	password = db.Column(db.String(250), nullable=False)
+
+
+db.init_app(app)
+
+
+with app.app_context():
+	db.create_all()
+
+
+@login_manager.user_loader
+def loader_user(user_id):
+	return Users.query.get(user_id)
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+	if request.method == "POST":
+		user = Users(username=request.form.get("username"),
+					password=request.form.get("password"))
+		db.session.add(user)
+		db.session.commit()
+		return redirect(url_for("login"))
+	return render_template("sign_up.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	error = None  
+	if request.method == "POST":
+		username = request.form.get("username")
+		password = request.form.get("password")
+		user = Users.query.filter_by(username=username).first()
+		if user is not None and user.password == password:
+			login_user(user)
+			return redirect(url_for("home"))
+		else:
+			error = "Nome de usuário ou senha incorretos. Tente novamente."
+	return render_template("login.html", error=error)
+
+
+
+@app.route("/logout")
+def logout():
+	logout_user()
+	return redirect(url_for("home"))
+
+
+@app.route("/")
+def home():
+	return render_template("home.html")
+
+
+if __name__ == "__main__":
+	app.run()
---
+++ Test.py
@@ -0,0 +1,37 @@
+import unittest
+from flask import Flask
+from Login import app, db, Users
+
+class TestLogin(unittest.TestCase):
+    def setUp(self):
+        # Configuração de teste
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test_db.sqlite"
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+
+        with app.app_context():
+            db.create_all()
+
+            # Adicione um usuário de teste à base de dados
+            test_user = Users(username="testuser", password="testpassword")
+            db.session.add(test_user)
+            db.session.commit()
+
+    def tearDown(self):
+        # Limpeza após os testes
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
+
+    def test_login_correct_credentials(self):
+        # Testa login com credenciais corretas
+        response = self.app.post("/login", data={"username": "testuser", "password": "testpassword"})
+        self.assertEqual(response.status_code, 302)  # Deve redirecionar após login bem-sucedido
+
+    def test_login_incorrect_credentials(self):
+        # Testa login com credenciais incorretas
+        response = self.app.post("/login", data={"username": "testuser", "password": "wrongpassword"})
+        self.assertEqual(response.status_code, 200)  # Deve permanecer na página de login
+
+if __name__ == "__main__":
+    unittest.main()
