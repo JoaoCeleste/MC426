1b89d91376dc64466c588d1ee4987779bf9449b0
Fernando <fernandomorato93@gmail.com>
2023-09-17 04:35:57 -0300

    Update models.py with new tables

--- models.py
+++ models.py
@@ -3,10 +3,36 @@ from database import db
 class User(db.Model):
     __tablename__ = 'user_'
 
-    id = db.Column(db.Integer, primary_key = True)
-
+    id = db.Column(db.Integer, primary_key=True)
 
 class Ingredient(db.Model):
     __tablename__ = 'ingredient'
 
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class Recipe(db.Model):
+    __tablename__ = 'recipe'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_.id'), nullable=False)
+    user = db.relationship('User', backref='recipes')
+
+class RecipeIngredient(db.Model):
+    __tablename__ = 'recipe_ingredient'
+
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
+    quantity = db.Column(db.Float, nullable=False)
+    unit = db.Column(db.String(20), nullable=False)
+
+class RecipeInstruction(db.Model):
+    __tablename__ = 'recipe_instruction'
+
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    step_number = db.Column(db.Integer, nullable=False)
+    instruction_text = db.Column(db.Text, nullable=False)
+
