a0e407a895d8bed6727d46896b76a58f0a0d66b1
Tiago Domingos Almeida Souza (tdas) <tiago.dsouza0@gmail.com>
2023-12-13 20:43:27 -0300

    fix: code-smells in main app

--- app.py
+++ app.py
@@ -8,38 +8,51 @@ from flask_bootstrap import Bootstrap5
 from flask_wtf.csrf import CSRFProtect
 
 
-def create_app(env='DEVELOPMENT'):
+def create_app():
+    """
+    Create and configure the Flask application.
+
+    This function initializes the Flask app, sets up configurations based on the environment,
+    initializes the database, sets up user authentication, registers blueprints for routes,
+    and configures additional extensions like migration and CSRF protection.
+
+    Returns:
+        Flask: The configured Flask application.
+    """
     app = Flask(__name__, template_folder='views')
+    env = environ.get('FLASK_ENV', 'DEVELOPMENT')
 
     if env == 'DEVELOPMENT':
         app.config.from_object('config.development')
     elif env == 'TESTING':
         app.config.from_object('config.testing')
+    try:
+        db.init_app(app)
 
-    db.init_app(app)
+        login_manager = LoginManager()
+        login_manager.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.init_app(app)
+        @login_manager.user_loader
+        def loader_user(user_id):
+            return User.query.get(user_id)
 
-    @login_manager.user_loader
-    def loader_user(user_id):
-        return User.query.get(user_id)
+        from routes.routes import routes
+        app.register_blueprint(routes)
 
-    from routes.routes import routes
-    app.register_blueprint(routes)
+        import models
+        with app.app_context():
+            db.create_all()
 
-    import models
-    with app.app_context():
-        db.create_all()
+        migrate = Migrate(app, db)
+        bootstrap = Bootstrap5(app)
+        if not env == 'TESTING':
+            csrf = CSRFProtect(app)
 
-    migrate = Migrate(app, db)
-    bootstrap = Bootstrap5(app)
-    if not env == 'TESTING':
-        csrf = CSRFProtect(app)
-
-    return app
+        return app
+    except Exception as e:
+        print(f"Error creating the app: {e}")
 
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
+    app.run(debug=False)
