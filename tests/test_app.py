6b40affab36c65dc3267aa207c616fcf7a7540a0
JoaoCeleste <j237441@dac.unicamp.br>
2023-12-14 19:14:44 -0300

    Testes Automatizados da AvaliacaoA5

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,8 +16,12 @@ class TestLogin(unittest.TestCase):
 
             # Adicione um usuário de teste à base de dados
             test_user = User(username="testuser", password="testpassword")
-            db.session.add(test_user)
+            test_user_2 = User(username="a", password="a")
+            test_user_3 = User(username="a"*12, password="a"*12)
+            db.session.add_all([test_user, test_user_2, test_user_3])
             db.session.commit()
+
+            
 
     def tearDown(self):
         # Limpeza após os testes
@@ -35,6 +39,60 @@ class TestLogin(unittest.TestCase):
         response = self.client.post("/login", data={"username": "testuser", "password": "wrongpassword"})
         self.assertEqual(response.status_code, 200)  # Deve permanecer na página de login
 
+    #Testes de Particionamento em Classes de Equivalencia
+
+    def test_login_min_password_length(self):
+        # Testa login com senha de tamanho minimo
+        response = self.client.post("/login", data={"username": "a", "password": "a"})
+        self.assertEqual(response.status_code, 302)  # Deve redirecionar após login bem-sucedido
+
+    def test_login_correct_password_length(self):
+        # Testa login com senha de tamanho medio
+        response = self.client.post("/login", data={"username": "testuser", "password": "testpassword"})
+        self.assertEqual(response.status_code, 302)  # Deve redirecionar após login bem-sucedido
+
+    def test_login_max_password_length(self):
+        # Testa login com senha de tamanho maximo
+        response = self.client.post("/login", data={"username": "a" * 12, "password": "a" * 12})
+        self.assertEqual(response.status_code, 302)  # Deve redirecionar após login bem-sucedido
+
+    def test_login_min_username_length(self):
+        # Testa login com username de tamanho minimo
+        response = self.client.post("/login", data={"username": "a", "password": "a"})
+        self.assertEqual(response.status_code, 302)  # Deve redirecionar após login bem-sucedido
+
+    def test_login_correct_username_length(self):
+        # Testa login com username de tamanho medio
+        response = self.client.post("/login", data={"username": "testuser", "password": "testpassword"})
+        self.assertEqual(response.status_code, 302)  # Deve redirecionar após login bem-sucedido
+
+    def test_login_max_username_length(self):
+        # Testa login com username de tamanho maximo
+        response = self.client.post("/login", data={"username": "a" * 12, "password": "a" * 12})
+        self.assertEqual(response.status_code, 302)  # Deve redirecionar após login bem-sucedido
+
+    #Testes de Analise de Valor Limite
+    
+    def test_login_below_min_password_length(self):
+        # Testa login com senha abaixo do tamanho minimo
+        response = self.client.post("/login", data={"username": "testuser", "password": ""})
+        self.assertEqual(response.status_code, 200)  # Deve permanecer na página de login
+
+    def test_login_above_max_password_length(self):
+        # Testa login com senha acima do tamanho maximo
+        response = self.client.post("/login", data={"username": "testuser", "password": "a" * 13})
+        self.assertEqual(response.status_code, 200)  # Deve permanecer na página de login
+
+    def test_login_below_min_username_length(self):
+        # Testa login com senha abaixo do tamanho minimo
+        response = self.client.post("/login", data={"username": "", "password": "testpassword"})
+        self.assertEqual(response.status_code, 200)  # Deve permanecer na página de login
+
+    def test_login_above_max_username_length(self):
+        # Testa login com senha acima do tamanho maximo
+        response = self.client.post("/login", data={"username": "a" * 13, "password": "testpassword"})
+        self.assertEqual(response.status_code, 200)  # Deve permanecer na página de login
+    
 
 if __name__ == "__main__":
     unittest.main()
